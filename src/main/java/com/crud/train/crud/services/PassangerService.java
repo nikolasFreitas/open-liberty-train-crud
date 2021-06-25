3d86c8cd521c3e184d9d681a89077fe8be173cc6
Nikolas Freitas <nikolas.freitas@philips.com>
2021-06-25 00:04:05 -0300

    :bug: Fix the service of passanger to create passanger

--- src/main/java/com/crud/train/crud/services/PassangerService.java
+++ src/main/java/com/crud/train/crud/services/PassangerService.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.persistence.NoResultException;
 
 import com.crud.train.crud.repository.Entity.Passanger;
 import com.crud.train.crud.repository.dao.PassangerDAO;
@@ -15,11 +16,15 @@ public class PassangerService {
   private PassangerDAO passangerDao;
   
   public UUID create(Passanger newPassanger) {
-    Boolean passangerAlreadyExists = passangerDao.find("email", newPassanger.getEmail()) != null;
-    if (passangerAlreadyExists) {
-      return null;
+    try {
+      Boolean passangerAlreadyExists = passangerDao.find("email", newPassanger.getEmail()) != null;
+      if (passangerAlreadyExists) {
+        return null;
+      }
+    } catch (NoResultException e) {
+      System.out.println(e);
     }
-    return UUID.fromString(passangerDao.create(newPassanger).getId());
+    return UUID.fromString(passangerDao.create(newPassanger).getUuid());
   }
   
   public PassangerDAO getDao() {
