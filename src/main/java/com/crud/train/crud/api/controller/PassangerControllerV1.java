9913407849bb8417d2585f7e920e2ad8c6cc1edd
Nikolas Freitas <nikolas.freitas@philips.com>
2021-06-24 23:44:32 -0300

    Add get passanger endpoint and enhance passanger creation response when occur duplicity error

--- src/main/java/com/crud/train/crud/api/controller/PassangerControllerV1.java
+++ src/main/java/com/crud/train/crud/api/controller/PassangerControllerV1.java
@@ -1,12 +1,19 @@
 package com.crud.train.crud.api.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.crud.train.crud.api.dto.CreatePassangerDTO;
 import com.crud.train.crud.repository.Entity.Passanger;
@@ -37,6 +44,34 @@ public class PassangerControllerV1 {
     passanger.setName(passangerDTO.getName());
     passanger.setEmail(passangerDTO.getEmail());
     var passangerUuid = passangerService.create(passanger);
-    return responseUtil.UUIDResponseFormat(passangerUuid);
+    if (passangerUuid != null) {
+      return responseUtil.UUIDResponseFormat(passangerUuid);
+    }
+
+    List<String> errorList = new ArrayList<>();
+    errorList.add("E-mail is not available");
+
+    return responseUtil.formatBadRequest(errorList);
+  }
+
+  @GET
+  @Path("/{uuid}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getPassanger(@PathParam("uuid") String passangerUuid) {
+    try {
+      Passanger passanger = passangerService.getPassanger(UUID.fromString(passangerUuid));
+      if (passanger != null) {
+        return responseUtil.customFormat(Status.OK, passanger);
+      }
+    } catch (IllegalArgumentException e) {
+      List<String> messageList = new ArrayList<>();
+      messageList.add(e.getMessage());
+
+      return responseUtil.formatBadRequest(messageList);
+    }
+
+
+    String notFoundMessage = "User with ID: " + passangerUuid + " not found";
+    return responseUtil.notFoundFormat(notFoundMessage);
   }
 }
