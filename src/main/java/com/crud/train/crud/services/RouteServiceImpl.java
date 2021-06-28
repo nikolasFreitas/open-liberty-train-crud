72400278f54a2505bf6a41cf8c5f84e41011f27b
Nikolas Freitas <nikolas.freitas@philips.com>
2021-06-28 01:40:36 -0300

    Enhance the route creation to prevent duplication

--- src/main/java/com/crud/train/crud/services/RouteServiceImpl.java
+++ src/main/java/com/crud/train/crud/services/RouteServiceImpl.java
@@ -1,5 +1,9 @@
 package com.crud.train.crud.services;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -12,10 +16,28 @@ public class RouteServiceImpl implements RouteService {
   @Inject
   private RouteDAO routeDao;
 
-  public Route create(Route travel) {
-    return routeDao.create(travel);
+  @Override
+  public Route create(Route route) {
+    Map<String, String> filterItems = new HashMap<>();
+    filterItems.put("destinyCity", route.getDestinyCity());
+    filterItems.put("originCity", route.getOriginCity());
+
+    Optional<Route> routeOption = routeDao.find(filterItems);
+    if (routeOption.isPresent()) {
+      var repeatedRoute = routeOption.get();
+      StringBuffer repeatedRouteLog = new StringBuffer("Route with")
+        .append(" destiny city: " + repeatedRoute.getDestinyCity())
+        .append(" and origin city: " + repeatedRoute.getOriginCity());
+
+      System.out.println(repeatedRouteLog);
+
+      return repeatedRoute;
+    }
+
+    return routeDao.create(route);
   }
 
+  @Override
   public RouteDAO getDao() {
     return routeDao;
   }
